86cd7ae1fbb93006c6e748bf58d666a3067946b0
auntim <hossennull@gmail.com>
2025-01-30 01:43:16 +0600

    implement allAdded page responsive

--- src/Shared/Allpets/Allpets.jsx
+++ src/Shared/Allpets/Allpets.jsx
@@ -88,13 +88,13 @@ const AllPets = () => {
             <Helmet>
                 <title>Pet | AddedPets</title>
             </Helmet>
-            <h2 className="text-4xl text-center font-bold text-orange-600 mb-6 ">--- All Added Pets are here---</h2>
+            <h2 className="text-2xl lg:text-4xl text-center font-bold text-orange-600 mb-6 ">--- All Added Pets are here---</h2>
             {loading ? (
                 <p>Loading...</p>
             ) : (
                 <div className="overflow-x-auto">
-                    <table className="min-w-full table-auto border-collapse border border-gray-300">
-                        <thead>
+                    <table className="min-w-full table-auto  ">
+                        <thead className="hidden md:table-header-group">
                             <tr className="bg-gray-100 dark:text-black">
                                 <th className="border border-gray-300 px-4 py-2">Image</th>
                                 <th className="border border-gray-300 px-4 py-2">Name</th>
@@ -107,47 +107,37 @@ const AllPets = () => {
                         </thead>
                         <tbody>
                             {pets.map((pet) => (
-                                <tr key={pet._id}>
-                                    <td className="border border-gray-300 px-4 py-2">
-                                        <img
-                                            src={pet.image}
-                                            alt={pet.name}
-                                            className="h-16 w-16 object-cover rounded-full"
-                                        />
+                                <tr key={pet._id} className="block md:table-row mb-4 md:mb-0 border-2 border-y-orange-300 md:border-gray-400">
+                                    <td className="block md:table-cell px-4 py-2">
+                                        <img src={pet.image} alt={pet.name} className="h-16 w-16 items-centerobject-cover rounded-full" />
                                     </td>
-                                    <td className="border border-gray-300 px-4 py-2">{pet.name}</td>
-                                    <td className="border border-gray-300 px-4 py-2">{pet.age}</td>
-                                    <td className="border border-gray-300 px-4 py-2">{pet.category}</td>
-                                    <td className="border border-gray-300 px-4 py-2">{pet.location}</td>
-                                    <td className="border border-gray-300 px-4 py-2">
+                                    <td className="block md:table-cell px-4 py-2 font-semibold">{pet.name}</td>
+                                    <td className="block md:table-cell px-4 py-2">{pet.age}</td>
+                                    <td className="block md:table-cell px-4 py-2">{pet.category}</td>
+                                    <td className="block md:table-cell px-4 py-2">{pet.location}</td>
+                                    <td className="block md:table-cell px-4 py-2">
                                         {pet.adopted ? "Adopted" : "Not Adopted"}
                                     </td>
-                                    <td className="border border-gray-300 px-4 py-2">
+                                    <td className=" md:table-cell px-4 py-2 flex gap-2 space-x-2 border-t-2 md:border-l-2">
                                         <button
                                             onClick={() => toggleAdoptionStatus(pet._id, pet.adopted)}
-                                            className={`px-3 py-1 rounded ${pet.adopted ? "bg-green-500" : "bg-red-500"
-                                                } text-white mr-2`}
+                                            className={`px-3 py-1 rounded ${pet.adopted ? "bg-green-500" : "bg-red-500"} text-white`}
                                         >
                                             {pet.adopted ? "Mark Not Adopted" : "Mark Adopted"}
                                         </button>
                                         <Link to={`/dashboard/update-pet/${pet._id}`}>
-                                            <button
-
-                                                className="px-3 py-2 mr-2 bg-slate-700 text-white rounded"
-                                            >
-                                                <FaPen></FaPen>
+                                            <button className="px-3 py-2 h-full bg-slate-700 text-white rounded">
+                                                <FaPen />
                                             </button>
                                         </Link>
-                                        <button
-                                            onClick={() => handleDelete(pet._id)}
-                                            className="px-3 py-2 bg-red-500 text-white rounded"
-                                        >
-                                            <FaTrashAlt></FaTrashAlt>
+                                        <button onClick={() => handleDelete(pet._id)} className="px-3 py-2 bg-red-500 text-white rounded">
+                                            <FaTrashAlt />
                                         </button>
                                     </td>
                                 </tr>
                             ))}
                         </tbody>
+
                     </table>
                 </div>
             )}
